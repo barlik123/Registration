acdabc37e7cf68777821b555959b51129d5497e2
Bar Likwornik <Bar.likwornik@gmail.com>
2025-05-12 19:20:40 -0400

    added animation and centered the thank you page

--- frontend/src/Thanks.jsx
+++ frontend/src/Thanks.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react' 
 import { Link } from "react-router-dom";
+import './Thanks.css'
 
 // Thanks component to display a thank you message after user registration
 function Thanks() {
     return (
-        <div>
-            <h1>Thank You!</h1>
+        <div className='thanks-container'>
+            <h1 className="thanks-title">Thank You!</h1>
             <p>Your submission has been received.</p>
             <p>
                 <Link to="/"><button>Go to home Page</button></Link>
